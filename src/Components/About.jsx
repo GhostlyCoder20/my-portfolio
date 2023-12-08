cc2d8e5fbb3a9071549755a7a8bfd689f15b7cae
Francisco Molina <119901915+GhostlyCoder20@users.noreply.github.com>
2023-12-08 23:33:13 +0000

    second commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I work as a software developer currently, I like to do anything that is creatively interesting with a programming language.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Teamwork",
+  "Work under pressure",
+  "Backend experience",
+  "Problem detection",
+  "Fast learner",
+  "Job Responsibility",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am interested in everything that I do not understand in programming because I want to master it in its entirety. I am constantly looking for anything new or unknown to me in order to improve my knowledge and skills.";
 
 const About = () => {
   return (
